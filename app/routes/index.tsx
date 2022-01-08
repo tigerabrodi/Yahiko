10ff13117f90e5670ffa9f4a990a3cbffa92cae8
tigerabrodi <tigerabrodi@gmail.com>
2022-01-08 17:29:59 +0100

    add home page title

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,34 @@
+import { Typography, Container } from '@mui/material'
+import { colors } from '~/theme'
+
 export default function Index() {
-  return <div>Hello World</div>
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <Typography
+        variant="h1"
+        marginTop={3}
+        fontSize={40}
+        fontWeight="bold"
+        color={colors.grey.light}
+      >
+        Yahiko
+      </Typography>
+      <Typography
+        color={colors.grey.light}
+        paragraph
+        marginTop={2}
+        textAlign="center"
+        fontSize={18}
+      >
+        Write and share documents in real-time!
+      </Typography>
+    </Container>
+  )
 }
